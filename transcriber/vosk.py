0a3fba6088bca926175a2ee0d6f4161c7df57dd3
oliebe <leoliebelt@gmail.com>
2026-09-07 12:35:27 -0300

    transcriber/vosk: automatic model download
    - Implements automatic downloading of chosen model with -tcl
    - New option -tcll to list available languages

--- transcriber/vosk.py
+++ transcriber/vosk.py
@@ -1,21 +1,27 @@
 from interfaces import Transcriber
 import sounddevice as sd
-from vosk import Model, KaldiRecognizer
+from vosk import Model, KaldiRecognizer, list_languages
 import queue
 import json
+import sys
 
 class VoskTranscriber(Transcriber):
 	description = "Vosk Offline Speech to Text. Supports more than 20 languages and very lightweight."
 	depends = "vosk, sounddevice"
 	def __init__(self, args):
 		self.args = args
-		self.model = Model("model")
 		self.sample_rate = 16000
 		self.audio_queue = queue.Queue()
 		self.device = sd.default.device
 
+		if self.args.tcll:
+			print("Available Vosk Languages:")
+			list_languages()
+			sys.exit()
 		#TODO: probably create a configuration spec for these kinds of function
 		self.source_choose()
+		self.model = Model(lang=self.args.tcl or "en-us")
+			
 		
 	def audio_callback(self, indata, frames, time_info, status):
 		self.audio_queue.put(bytes(indata))
@@ -51,4 +57,8 @@ class VoskTranscriber(Transcriber):
 	
 	@staticmethod
 	def make_arguments(parser: ArgumentParser):
-		parser.add_argument("-tcl", help="Vosk: Choose the transcribing language")
+		parser.add_argument("-tcl", 
+					  help="Vosk: Change the transcribing language (see -tcll output) (default: en-us)")
+		parser.add_argument("-tcll", 
+							help="Vosk: List available languages and terminate",
+							action="store_true")
